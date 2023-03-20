014fc4fe1467d2b58a4b08ff85e47c3a49c1634d
wiligetsurvey <wili@getsurvey.id>
2023-03-20 10:50:34 +0800

    rubah tag tab

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,12 +27,12 @@ const Interior = () => {
     <ThemeProvider theme={interiorTheme}>
       <Fragment>
         <Head>
-          <title>Interior | A react next landing page</title>
+          <title>PT. Surya Chandra Engineering</title>
           <meta name="theme-color" content="#171717" />
-          <meta name="description" content="React next landing page" />
+          <meta name="description" content="PT. Surya Chandra Engineering" />
           <meta
             name="keywords"
-            content="React, React js, Next, Next js, Gatsby, Gatsby Js, Fast Landing, Modren Landing"
+            content="PT. Surya Chandra Engineering"
           />
           <link
             href="https://fonts.googleapis.com/css?family=Raleway:500,600&display=swap"
@@ -62,6 +62,16 @@ const Interior = () => {
             {/* <News /> */}
             {/* <Testimonial /> */}
             <Gallery2 />
+            <div className="pt-[100px]">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3944.597797649671!2d115.27774141478382!3d-8.634553893795086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zOMKwMzgnMDQuNCJTIDExNcKwMTYnNDcuOCJF!5e0!3m2!1sen!2sid!4v1678616261139!5m2!1sen!2sid"
+                width="100%"
+                height="450"
+                allowfullscreen=""
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
           </ContentWrapper>
           <Footer />
         </InteriorWrapper>
